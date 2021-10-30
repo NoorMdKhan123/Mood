45392d2e488aa37261d8cc408f8234db94c908b1
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-30 23:58:54 +0530

    Done with refactor code

--- MoodPrgMSTest/UnitTest1.cs
+++ MoodPrgMSTest/UnitTest1.cs
@@ -28,11 +28,11 @@ namespace MoodPrgMSTest
             //Arrange
             string except = "SAD";
             string message = "I am in Sad mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
             
 
             //Act
-            var mood = moodAnalyser.AnalyseMood(message);
+            var mood = moodAnalyser.AnalyseMood();
 
             //Assert
             Assert.AreEqual(except, mood);
